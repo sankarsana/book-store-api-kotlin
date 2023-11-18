3cd3d800eb72dea0b10021617dbd964d8d009d56
s.a.demyanov <s.a.demyanov@tinkoff.ru>
2023-11-18 17:05:53 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "nrs.sankarsana.book-store"
---
+++ src/test/kotlin/nrs/sankarsana/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package nrs.sankarsana
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import nrs.sankarsana.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
